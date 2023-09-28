0ba1dfc7ab551c5601bcda83fc4ea5e5264704e5
Eddy Adegnandjou <adegnandjoueddy12@gmail.com>
2023-09-28 09:32:50 +0100

    adding registration

--- account/urls.py
+++ account/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     #      name='password_reset_complete'),
     path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
 ]
