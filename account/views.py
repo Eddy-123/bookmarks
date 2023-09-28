eabd2f1cfc2eafe01a433ae13f05737b86b40d53
Eddy Adegnandjou <adegnandjoueddy12@gmail.com>
2023-09-28 14:22:16 +0100

    display error of success messages for user profile edit view

--- account/views.py
+++ account/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile
+from django.contrib import messages
 
 def user_login(request):
     if request.method == 'POST':
@@ -63,6 +64,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
