5d8137aa757c52194774e35298251a14664c7013
Eddy Adegnandjou <adegnandjoueddy12@gmail.com>
2023-09-30 09:08:53 +0100

    customizing authentication pipeline

--- account/authentication.py
+++ account/authentication.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from account.models import Profile
 
 class EmailAuthBackend:
     def authenticate(self, request, username=None, password=None):
@@ -15,3 +16,11 @@ class EmailAuthBackend:
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
             return None
+
+def create_profile(backend, user, *args, **kwargs):
+    """
+    Create user profile for authentication
+    """
+    Profile.objects.get_or_create(user=user)
+
+    
--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -156,3 +156,16 @@ SOCIAL_AUTH_TWITTER_SECRET = os.getenv("SOCIAL_AUTH_TWITTER_SECRET")
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.getenv("SOCIAL_AUTH_GOOGLE_OAUTH2_KEY")
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.getenv("SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET")
+
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+]
