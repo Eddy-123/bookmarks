d1925e5df4e4e7dd7ba1b8af0c8ddb61fc099f21
Eddy Adegnandjou <adegnandjoueddy12@gmail.com>
2023-10-04 00:03:26 +0100

    bookmarklet functionnality completed

--- images/views.py
+++ images/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ImageCreateForm
+from .models import Image
 
 @login_required
 def image_create(request):
@@ -21,3 +22,10 @@ def image_create(request):
                   'images/image/create.html',
                   {'section': 'images',
                    'form': form})
+
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(request, 
+                  'images/image/detail.html',
+                  {'section': 'images', 
+                   'image':image})
