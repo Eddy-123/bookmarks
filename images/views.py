9f45ac7462b2c2f33d10e8582054e1742aea5f4f
Eddy Adegnandjou <adegnandjoueddy12@gmail.com>
2023-10-04 22:50:25 +0100

    like/unlike feature

--- images/views.py
+++ images/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ImageCreateForm
 from .models import Image
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 
 @login_required
 def image_create(request):
@@ -29,3 +31,20 @@ def image_detail(request, id, slug):
                   'images/image/detail.html',
                   {'section': 'images', 
                    'image':image})
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except Image.DoesNotExist:
+            pass
+    return JsonResponse({'status': 'error'})
